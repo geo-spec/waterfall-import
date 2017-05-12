a2d51f14c9bd396b87b736bd99f3f29f8061a94a
Shilin <g.shilin@skoltech.ru>
2017-05-12 17:31:29 +0300

    material-ui

--- src/components/Import.js
+++ src/components/Import.js
@@ -6,17 +6,20 @@ import React from 'react';
 let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+
+
+
       </div>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
 
 export default AppComponent;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/Main';
+import App from './components/Import';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+
+
+import injectTapEventPlugin from 'react-tap-event-plugin'
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<MuiThemeProvider><App /></MuiThemeProvider>,
+document.getElementById('app'));
